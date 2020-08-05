4ecfde4ad0d4ea59363bb74b8f0a4a4e53df6b6f
Jason Frame <jwf@jasonframe.co.uk>
2020-08-05 18:15:43 +0100

    something

--- config.go
+++ config.go
@@ -3,3 +3,15 @@ package main
 type Config struct {
 	Port int `json:"port"`
 }
+
+func defaultConfig() *Config {
+	return &Config{
+		Port: 8080,
+	}
+}
+
+func blankConfig() *Config {
+	return &Config{
+		Port: -1,
+	}
+}
